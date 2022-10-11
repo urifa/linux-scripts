47b25178524f7d95e0cc4615c725c875f4af4bb3
urifa <109032628+urifa@users.noreply.github.com>
2022-10-11 18:02:59 +0200

    Add ubuntu-initial-setup.sh file and create the basic structure

---
+++ ubuntu-initial-setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check for updates
+
+# Install package updates
+
+# Remove not needed package dependencies and clear out local repository of retrieved package files
+
+# Enable ufw firewall
+
+# Install basic applications and tools
+
+# Install basic snap applications and tools
+
+# Enable minimize on click preference in Ubuntu Dock
