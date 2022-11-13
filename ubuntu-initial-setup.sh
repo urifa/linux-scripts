96bd6f963ee98d1fadbdab6afc8b4657de2a9ff7
urifa <109032628+urifa@users.noreply.github.com>
2022-11-13 19:36:22 +0100

    Add the main content on ubuntu-initial-setup.sh file

--- ubuntu-initial-setup.sh
+++ ubuntu-initial-setup.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 # Check for updates
+sudo apt update
 
 # Install package updates
+sudo apt -y upgrade
 
 # Remove not needed package dependencies and clear out local repository of retrieved package files
+sudo apr -y autoremove
+sudo apt autoclean
 
 # Enable ufw firewall
+sudo ufw enable
 
 # Install basic applications and tools
+sudo apt -y install vlc gimp gnome-builder clamav chkrootkit lynis
 
 # Install basic snap applications and tools
+sudo snap install chromium brave
 
 # Enable minimize on click preference in Ubuntu Dock
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
